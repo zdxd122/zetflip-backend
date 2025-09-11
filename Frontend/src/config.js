87094ee9897650aca324b8fe16cb5cf7add7c96e
ZetFlip Developer <developer@zetflip.com>
2025-09-12 02:20:02 +0400

    Remove hCaptcha secret from config, use environment variables

--- Frontend/src/config.js
+++ Frontend/src/config.js
@@ -2,5 +2,5 @@
 
 export default {
   api: "https://zetflip-backend.onrender.com",
-  h_captcha_key: "ES_f59302c7723245a58b30799e14d7ffad",
+  h_captcha_key: import.meta.env.VITE_HCAPTCHA_KEY || "ES_f59302c7723245a58b30799e14d7ffad",
 };
